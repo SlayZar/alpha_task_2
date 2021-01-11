90b1f88b35ea03410135bd499435b59ad6fd93c4
Dmitry Vaskin <vaskind@yandex.ru>
2021-01-11 15:10:29 +0300

    Update preparation_funcs.py

--- preparation_funcs.py
+++ preparation_funcs.py
@@ -149,8 +149,10 @@ def boost_scor(t_s, test_targ, cols_path, model_path, cats_path, output_path):
 def nn_scoring(path_to_test_dataset, path_to_checkpoints, model_name, result_path, device):
   dir_with_test_datasets = os.listdir(path_to_test_dataset)
   dataset_test = sorted([os.path.join(path_to_test_dataset, x) for x in dir_with_test_datasets])
-
-  model = TransactionsRnn(transaction_features, embedding_projections, top_classifier_units=128).to(device)
+  if 'nn_model_3' in model_name:
+    model = TransactionsRnn2(transaction_features, embedding_projections, top_classifier_units=64).to(device)
+  else:
+    model = TransactionsRnn(transaction_features, embedding_projections, top_classifier_units=128).to(device)
   model.load_state_dict(torch.load(os.path.join(path_to_checkpoints, model_name)))
   test_preds = inference(model, dataset_test, batch_size=128, device=device)
   test_preds.to_csv(result_path, index=None)
@@ -165,6 +167,63 @@ class TransactionsRnn(nn.Module):
                                                                                             padding_idx=None) 
                                                           for feature in transactions_cat_features])
         self._spatial_dropout = nn.Dropout2d(0.05)
+        self._transaction_cat_embeddings_concated_dim = sum([embedding_projections[x][1] for x in transactions_cat_features])
+        
+        self._product_embedding = self._create_embedding_projection(*embedding_projections[product_col_name], padding_idx=None)
+        
+        self._gru = nn.GRU(input_size=self._transaction_cat_embeddings_concated_dim,
+                             hidden_size=rnn_units, batch_first=True, bidirectional=True)
+        
+        self._hidden_size = rnn_units
+        
+        # построим классификатор, он будет принимать на вход: 
+        # [max_pool(gru_states), avg_pool(gru_states), product_embed]
+        pooling_result_dimension = self._hidden_size * 2
+         
+        self._top_classifier = nn.Sequential(nn.Linear(in_features=2*pooling_result_dimension + 
+                                                       embedding_projections[product_col_name][1], 
+                                                       out_features=top_classifier_units),
+                                             nn.ReLU(),
+                                             nn.Linear(in_features=top_classifier_units, out_features=1)
+                                            )
+        
+    def forward(self, transactions_cat_features, product_feature):
+        batch_size = product_feature.shape[0]
+        
+        embeddings = [embedding(transactions_cat_features[i]) for i, embedding in enumerate(self._transaction_cat_embeddings)]
+        concated_embeddings = torch.cat(embeddings, dim=-1)
+        concated_embeddings = concated_embeddings.permute(0, 2, 1).unsqueeze(3)
+        
+        dropout_embeddings = self._spatial_dropout(concated_embeddings)
+        dropout_embeddings = dropout_embeddings.squeeze(3).permute(0, 2, 1)
+
+        states, _ = self._gru(dropout_embeddings)
+        
+        rnn_max_pool = states.max(dim=1)[0]
+        rnn_avg_pool = states.sum(dim=1) / states.shape[1]        
+        
+        product_embed = self._product_embedding(product_feature)
+                
+        combined_input = torch.cat([rnn_max_pool, rnn_avg_pool, product_embed], dim=-1)
+            
+        logit = self._top_classifier(combined_input)        
+        return logit
+    
+    @classmethod
+    def _create_embedding_projection(cls, cardinality, embed_size, add_missing=True, padding_idx=0):
+        add_missing = 1 if add_missing else 0
+        return nn.Embedding(num_embeddings=cardinality+add_missing, embedding_dim=embed_size, padding_idx=padding_idx)
+
+
+class TransactionsRnn2(nn.Module):
+    def __init__(self, transactions_cat_features, embedding_projections, product_col_name='product', 
+                 rnn_units=128, top_classifier_units=32):
+        super(TransactionsRnn, self).__init__()
+        
+        self._transaction_cat_embeddings = nn.ModuleList([self._create_embedding_projection(*embedding_projections[feature], 
+                                                                                            padding_idx=None) 
+                                                          for feature in transactions_cat_features])
+        self._spatial_dropout = nn.Dropout2d(0.1)
         self._transaction_cat_embeddings_concated_dim = sum([embedding_projections[x][1] for x in transactions_cat_features])
         
         self._product_embedding = self._create_embedding_projection(*embedding_projections[product_col_name], padding_idx=None)
